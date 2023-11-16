e2ca5f47405cea6c52924ca829c75778bdc1e825
nicolas-mangini <nicolas.mangini@ing.austral.edu.ar>
2023-11-16 02:11:16 -0300

    fix test class

--- app/src/main/java/edu/austral/dissis/checkers/builder/CheckersBoardBuilder.java
+++ app/src/main/java/edu/austral/dissis/checkers/builder/CheckersBoardBuilder.java
@@ -14,6 +14,6 @@ public class CheckersBoardBuilder implements BoardBuilder {
     }
 
     public CheckersBoardBuilder() {
-        this.checkersPieceBuilder = new TESTCheckersPieceBuilder();
+        this.checkersPieceBuilder = new CheckersPieceBuilder();
     }
 }
