6bb3bceea160e9a8799d4422891b40fa5f964eda
nicolas-mangini <nicolas.mangini@ing.austral.edu.ar>
2023-11-16 01:34:40 -0300

    refactor

--- app/src/main/java/edu/austral/dissis/checkers/validator/game/PiecesRemainingValidator.java
+++ app/src/main/java/edu/austral/dissis/checkers/validator/game/PiecesRemainingValidator.java
@@ -8,22 +8,26 @@ import edu.austral.dissis.common.util.WinResult;
 import edu.austral.dissis.common.validator.GameOverValidator;
 import lombok.AllArgsConstructor;
 
+/**
+ * Validator to check if the game is over based on the number of remaining pieces of the opponent.
+ */
 @AllArgsConstructor
 public class PiecesRemainingValidator implements GameOverValidator {
-    private final int amount;
+    private final int maxRemainingPieces;
 
-    //end game when enemy has less or equal pieces than amount
     @Override
     public WinResult<Boolean, Colour> isGameOver(Movement movement, Board board, GameManager gameManager) {
         Colour enemyColour = gameManager.getTurnChanger().getCurrentTurn();
 
-        int piecesRemaining = (int) board.getTiles()
+        int remainingPieces = (int) board.getTiles()
                 .stream()
                 .filter(tile -> tile.getPiece() != null)
                 .filter(tile -> tile.getPiece().getColour().equals(enemyColour))
                 .count();
 
-        if (piecesRemaining <= amount) return new WinResult<>(true, enemyColour);
-        else return new WinResult<>(false, null);
+        if (remainingPieces <= maxRemainingPieces)
+            return new WinResult<>(true, enemyColour);
+        else
+            return new WinResult<>(false, null);
     }
 }
